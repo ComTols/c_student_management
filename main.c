c097a63716f9b3c9f1632e44f72855c5b00cc6e7
Maximilian Schüller <comtols@outlook.de>
2022-11-28 15:08:40 +0100

    Changes

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct {
+    int tag;
+    int monat;
+    int jahr;
+} Datum;
 
+typedef struct {
+    char *vname;
+    char *nname;
+    int id;
+    Datum gtag;
+    Datum beginn;
+    Datum ende;
 } Student;
 
 void saveAsFile(Student* students, char* path) {
@@ -13,9 +25,30 @@ Student* getFromFile(char* path) {
 
 }
 
+bool decode(Student *students){
+    char json = malloc(374);
+    for (int i = 0; i < ((sizeof *students)/(sizeof students[0])); ++i) {
+
+    }
+}
+
+Student* encode(char* jsonString) {
+    char* index = jsonString;
+    printf("%c", jsonString[0]);
+}
+
+void jObject() {
+
+}
+
+int main(int argc, char *argv[]) {
+    Student* students = {
+            {"Max", "Mustermann", 334562, {10,11,2012},{10,11,2012},{10,11,2012}},
+            {"Adam", "Musterfrau", 234566, {2,8,2010},{10,11,2012},{10,11,2012}}
+    };
 
 
-int main(int argc, char* argv[]) {
-    Student* students = malloc(100*sizeof (Student));
+    //saveAsFile(students, "database.json");
+    encode("Test");
     return 0;
 }
