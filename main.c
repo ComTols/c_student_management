5a1cb1e4dc8a7ad1ccf9ceb9ff97b346e63ec088
Maximilian Schüller <comtols@outlook.de>
2022-11-28 11:13:21 +0100

    Rough structure of the file

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+
+} Student;
+
+void saveAsFile(Student* students) {
+
+}
+
+Student* getFromFile() {
+
+}
+
+
+
+int main() {
+    Student* students = malloc(100*sizeof (Student));
+    return 0;
+}
