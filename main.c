a9d9ab79963ebb54f5d597c6c66102b411d21007
Maximilian Schüller <comtols@outlook.de>
2022-12-01 14:54:36 +0100

    Short code of function "menuAnzeige"

--- main.c
+++ main.c
@@ -3,237 +3,88 @@
 #include <stdlib.h>
 #include <windows.h>
 
-
 COORD coord = { 0, 0 };
 
-void gotoxy(int x, int y) {
+void goToXY(int x, int y) {
     coord.X = x; coord.Y = y;
 
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-
-
-
-
-int menuAnzeige(int MenuChoice,int WhereInMenu)
-{
-    printf("\33[?25l");   //Versteckt den Courser   (um ihn wieder zu aktivieren printf("\33[?25h");
-
-    switch (MenuChoice)
+int menuAnzeige(int MenuChoice)
     {
-        case 1:
-            system ("cls");
-
-
-            gotoxy(17, 5);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 STUDENTEN-MANAGEMENT-PROGRAMM \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(17, 19);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(26, 7);
-            printf("\033[0;31m");       //Auswahl 1 gefärbt
-            printf("1. Sch\x81ler eintragen\n\n");
-            printf("\033[0m");
-
-            gotoxy(26, 9);
-            printf("2. Sch\x81ler hinzuf\x81gen\n\n");
-
-            gotoxy(26, 11);
-            printf("3. Anzahl der Sch\x81ler\n\n");
-
-            gotoxy(26, 13);
-            printf("4. Zeige Sch\x81ler \n\n");
-
-            gotoxy(26, 15);
-            printf("5. Zeige alle Sch\x81ler\n\n");
-
-            gotoxy(26, 17);
-            printf("6. L\x94sche Sch\x81ler\n\n");
-
-            gotoxy(21, 21);
-            printf(" Dr\x81 \bcke ESC zum speichern und verlassen");
-
-            break;
-
-        case 2:
-            system ("cls");
-
-            gotoxy(17, 5);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 STUDENTEN-MANAGEMENT-PROGRAMM \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(17, 19);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(26, 7);
-            printf("1. Sch\x81ler eintragen\n\n");
-
-            printf("\033[0;31m");       //Auswahl 2 gefärbt
-            gotoxy(26, 9);
-            printf("2. Sch\x81ler hinzuf\x81gen\n\n");
-            printf("\033[0m");
-
-            gotoxy(26, 11);
-            printf("3. Anzahl der Sch\x81ler\n\n");
-
-            gotoxy(26, 13);
-            printf("4. Zeige Sch\x81ler \n\n");
-
-            gotoxy(26, 15);
-            printf("5. Zeige alle Sch\x81ler\n\n");
-
-            gotoxy(26, 17);
-            printf("6. L\x94sche Sch\x81ler\n\n");
-
-            gotoxy(21, 21);
-            printf(" Dr\x81 \bcke ESC zum speichern und verlassen");
-
-            break;
-
-        case 3:
-            system ("cls");
-
-            gotoxy(17, 5);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 STUDENTEN-MANAGEMENT-PROGRAMM \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(17, 19);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(26, 7);
-            printf("1. Sch\x81ler eintragen\n\n");
-
-            gotoxy(26, 9);
-            printf("2. Sch\x81ler hinzuf\x81gen\n\n");
-
-            printf("\033[0;31m");       //Auswahl 3 gefärbt
-            gotoxy(26, 11);
-            printf("3. Anzahl der Sch\x81ler\n\n");
-            printf("\033[0m");
-
-            gotoxy(26, 13);
-            printf("4. Zeige Sch\x81ler \n\n");
-
-            gotoxy(26, 15);
-            printf("5. Zeige alle Sch\x81ler\n\n");
-
-            gotoxy(26, 17);
-            printf("6. L\x94sche Sch\x81ler\n\n");
-
-            gotoxy(21, 21);
-            printf(" Dr\x81 \bcke ESC zum speichern und verlassen");
-
-            break;
-
-        case 4:
-            system ("cls");
-
-            gotoxy(17, 5);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 STUDENTEN-MANAGEMENT-PROGRAMM \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(17, 19);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(26, 7);
-            printf("1. Sch\x81ler eintragen\n\n");
-
-            gotoxy(26, 9);
-            printf("2. Sch\x81ler hinzuf\x81gen\n\n");
-
-            gotoxy(26, 11);
-            printf("3. Anzahl der Sch\x81ler\n\n");
-
-            gotoxy(26, 13);
-            printf("\033[0;31m");       //Auswahl 4 gefärbt
-            printf("4. Zeige Sch\x81ler \n\n");
-            printf("\033[0m");
-
-            gotoxy(26, 15);
-            printf("5. Zeige alle Sch\x81ler\n\n");
-
-            gotoxy(26, 17);
-            printf("6. L\x94sche Sch\x81ler\n\n");
-
-            gotoxy(21, 21);
-            printf(" Dr\x81 \bcke ESC zum speichern und verlassen");
-
-            break;
-
-        case 5:
-            system ("cls");
-
-            gotoxy(17, 5);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 STUDENTEN-MANAGEMENT-PROGRAMM \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(17, 19);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(26, 7);
-            printf("1. Sch\x81ler eintragen\n\n");
-
-            gotoxy(26, 9);
-            printf("2. Sch\x81ler hinzuf\x81gen\n\n");
-
-            gotoxy(26, 11);
-            printf("3. Anzahl der Sch\x81ler\n\n");
-
-            gotoxy(26, 13);
-            printf("4. Zeige Sch\x81ler \n\n");
-
-            printf("\033[0;31m");       //Auswahl 5 gefärbt
-            gotoxy(26, 15);
-            printf("5. Zeige alle Sch\x81ler\n\n");
-            printf("\033[0m");
-
-            gotoxy(26, 17);
-            printf("6. L\x94sche Sch\x81ler\n\n");
-
-            gotoxy(21, 21);
-            printf(" Dr\x81 \bcke ESC zum speichern und verlassen.");
-
-            break;
-
-        case 6:
-            system ("cls");
-
-            gotoxy(17, 5);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 STUDENTEN-MANAGEMENT-PROGRAMM \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(17, 19);
-            printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-            gotoxy(26, 7);
-            printf("1. Sch\x81ler eintragen\n\n");
-
-            gotoxy(26, 9);
-            printf("2. Sch\x81ler hinzuf\x81gen\n\n");
-
-            gotoxy(26, 11);
-            printf("3. Anzahl der Sch\x81ler\n\n");
-
-            gotoxy(26, 13);
-            printf("4. Zeige Sch\x81ler \n\n");
-
-            gotoxy(26, 15);
-            printf("5. Zeige alle Sch\x81ler\n\n");
-
-            printf("\033[0;31m");       //Auswahl 6 gefärbt
-            gotoxy(26, 17);
-            printf("6. L\x94sche Sch\x81ler\n\n");
-            printf("\033[0m");
-
-            gotoxy(21, 21);
-            printf(" Dr\x81 \bcke ESC zum speichern und verlassen");
-
-            break;
-
-        default:
-            system ("cls");
-            printf("\nEaster Egg");
+        //Versteckt den Courser (um ihn wieder zu aktivieren printf("\33[?25h");
+        printf("\33[?25l");
+
+        system ("cls");
+        goToXY(17, 5);
+        printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 STUDENTEN-MANAGEMENT-PROGRAMM \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+
+        goToXY(26, 7);
+        printf("1. Sch\x81ler eintragen\n\n");
+
+        goToXY(26, 9);
+        printf("2. Sch\x81ler hinzuf\x81gen\n\n");
+
+        goToXY(26, 11);
+        printf("3. Anzahl der Sch\x81ler\n\n");
+
+        goToXY(26, 13);
+        printf("4. Zeige Sch\x81ler \n\n");
+
+        goToXY(26, 15);
+        printf("5. Zeige alle Sch\x81ler\n\n");
+
+        goToXY(26, 17);
+        printf("6. L\x94sche Sch\x81ler\n\n");
+
+        switch (MenuChoice)
+        {
+            case 1:
+                goToXY(26, 7);
+                printf("\033[0;31m");       //Auswahl 1 gefärbt
+                printf("1. Sch\x81ler eintragen\n\n");
+                printf("\033[0m");
+                break;
+            case 2:
+                printf("\033[0;31m");       //Auswahl 2 gefärbt
+                goToXY(26, 9);
+                printf("2. Sch\x81ler hinzuf\x81gen\n\n");
+                printf("\033[0m");
+                break;
+            case 3:
+                printf("\033[0;31m");       //Auswahl 3 gefärbt
+                goToXY(26, 11);
+                printf("3. Anzahl der Sch\x81ler\n\n");
+                printf("\033[0m");
+                break;
+            case 4:
+                goToXY(26, 13);
+                printf("\033[0;31m");       //Auswahl 4 gefärbt
+                printf("4. Zeige Sch\x81ler \n\n");
+                printf("\033[0m");
+                break;
+            case 5:
+                printf("\033[0;31m");       //Auswahl 5 gefärbt
+                goToXY(26, 15);
+                printf("5. Zeige alle Sch\x81ler\n\n");
+                printf("\033[0m");
+                break;
+
+            case 6:
+                printf("\033[0;31m");       //Auswahl 6 gefärbt
+                goToXY(26, 17);
+                printf("6. L\x94sche Sch\x81ler\n\n");
+                printf("\033[0m");
+                break;
+            default:
+                system ("cls");
+                printf("\nEaster Egg");
+        }
+        goToXY(21, 21);
+        printf(" Dr\x81 \bcke ESC zum speichern und verlassen");
+        return 0;
     }
-    return 0;
-}
-
 
 void CheckKey() {
     int WhereInMenu;
@@ -265,18 +116,16 @@ void CheckKey() {
                     {
                         WhereInMenu = 6;
                     }
-                    menuAnzeige(WhereInMenu,WhereInMenu);
+                    menuAnzeige(WhereInMenu);
                     break;
 
                 case 80:                 //80 ist KeyCodeValue für Down-Arrow
-
                     WhereInMenu++;
                     if (WhereInMenu > 6)
                     {
                         WhereInMenu = 1;
                     }
-                    menuAnzeige(WhereInMenu,WhereInMenu);
-
+                    menuAnzeige(WhereInMenu);
                     break;
             }
         }
@@ -284,9 +133,9 @@ void CheckKey() {
 
 
 int main() {
-    menuAnzeige(1,1);
+    menuAnzeige(1);
     while (1) {     //viele ressourcen
-                     CheckKey();
+        CheckKey();
         Sleep(10);   //spart Ressouren
-
-}}
+    }
+}
